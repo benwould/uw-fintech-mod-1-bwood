c99d653d8b148304b1ebf4d75f9e74d247d8715b
Ben Wood <benwould@gmail.com>
2022-10-27 11:27:33 -0700

    print loans, loan amount, average loans

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -12,44 +12,55 @@ First, let's start with some calculations on a list of prices for 5 loans.
     3. Using the sum of all loans and the total number of loans, calculate the average loan price.
     4. Print all calculations with descriptive messages.
 """
-loan_costs = [500, 600, 200, 1000, 450]
+loan_amount = [500, 600, 200, 1000, 450]
 
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
 # YOUR CODE HERE!
-
+num_loans = len(loan_amount)
+print(f"Number of loans is: {num_loans}")
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
-
+total_loan_amount = sum(loan_amount)
+print(f"Total loan amount is: ${total_loan_amount}")
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
-
+average_loan_amount = total_loan_amount/num_loans
+print(f"The average loan amount is: ${average_loan_amount}")
 """Part 2: Analyze Loan Data.
 
 Analyze the loan to determine the investment evaluation.
 
-Using more detailed data on one of these loans, follow these steps to calculate a Present Value, or a "fair price" for what this loan would be worth.
+Using more detailed data on one of these loans, follow these steps to calculate a Present Value, 
+or a "fair price" for what this loan would be worth.
 
-1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
+1. Use get() on the dictionary of additional information to extract the **Future Value** and 
+**Remaining Months** on the loan.
     a. Save these values as variables called `future_value` and `remaining_months`.
     b. Print each variable.
 
     @NOTE:
-    **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
+    **Future Value**: The amount of money the borrower has to pay back upon maturity of 
+    the loan (a.k.a. "Face Value")
     **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
 
-2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum required return of 20% as the discount rate.
-3. Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-    a. If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-    b. Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
+2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum 
+required return of 20% as the discount rate.
+3. Write a conditional statement (an if-else statement) to decide if the present value 
+represents the loan's fair value.
+    a. If the present value of the loan is greater than or equal to the cost, then print
+     a message that says the loan is worth at least the cost to buy it.
+    b. Else, the present value of the loan is less than the loan cost, then print a message 
+    that says that the loan is too expensive and not worth the price.
 
     @NOTE:
-    If Present Value represents the loan's fair value (given the required minimum return of 20%), does it make sense to buy the loan at its current cost?
+    If Present Value represents the loan's fair value (given the required minimum return of 20%), 
+    does it make sense to buy the loan at its current cost?
 """
 
 # Given the following loan data, you will need to calculate the present value for the loan
